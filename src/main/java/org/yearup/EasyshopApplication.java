dce9aa1b999c4075e5fe804b5b529bdbbff874ba
Brianna Castanon <bricastanon1@gmail.com>
2024-12-11 13:19:45 -0700

    Initial commit

---
+++ src/main/java/org/yearup/EasyshopApplication.java
@@ -0,0 +1,15 @@
+package org.yearup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyshopApplication
+{
+    public static void main(String[] args) {
+        SpringApplication.run(EasyshopApplication.class, args);
+    }
+}
+
+//"token": "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ4Z2FkbWluIiwiYXV0aCI6IlJPTEVfQURNSU4iLCJleHAiOjE3MzQwNDUxMzN9.A4IJ_uVa75OACtFE0WHzBEiZj2a5d-fQV0MSlGADD2qRwenLMJEyK6Iz2vluvugFOHDR-6iJ-3qsmUtfMnI9oQ",
+//"token": "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZXN0dXNlciIsImF1dGgiOiJST0xFX1VTRVIiLCJleHAiOjE3MzQwNDY1NjR9.BMUHMPMncGTuLi-v-Htp1OHYU0iIu1V4qxYzugCziz1HG8E8o_CULaO0LPCK8UGCYZtB2ONUZ1ufeOrvnukoiQ
